d80f1812f4b9880a7ce3f1e398f8bc247283fa83
unknown <arek91w@gmail.com>
2020-08-23 19:44:35 +0200

    Recommendations WORKING

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ root = Tk()
 root.title('VIEW 1')
 root.geometry('300x100')
 
-msft = yf.Ticker("MSFT")
 
-hist = msft.history(period="max")
+
+#hist = msft.history(period="max")
 #closing_pos = hist["Close"]
-print(hist.index)
+#print(hist.index)
 #msft = yf.Ticker("MSFT")
-print(msft.recommendations)
+#print(msft.recommendations)
 
 #print(msft.recommendations)
 e = Entry(root, width=50)
 e.pack()
 def getCloseOfIndex(dateList):
     myList = []
+    print(hist)
     for date in dateList:
         myVar = datetime.datetime(date.year, date.month, date.day)
         try:
-            myList.append(hist[hist.index == myVar]["Close"].values[0])
+            xx = hist[hist.index == myVar]["Close"].values[0]
         except:
-            myList.append(0)
+            xx = 0
+        myList.append(xx)
     #for date in datelist2:
         #print(hist[hist.index == date]["Close"])
        # myList.append(hist[hist.index == date]["Close"])
@@ -37,14 +39,14 @@ def getCloseOfIndex(dateList):
 
 def graph():
     myTicker = yf.Ticker(e.get())
+    global hist
     hist = myTicker.history(period="max")
     recomm = myTicker.recommendations
     yValues = getCloseOfIndex(recomm.index)
-    print(type(yValues[0]))
-    print(type(np.zeros(len(recomm))[0]))
-    print((np.zeros(len(recomm))).shape)
-    plt.plot(hist.index, hist["Close"])
-    plt.scatter(recomm.index, yValues, s=1, c="red")
+    print(recomm.index)
+    print(yValues)
+    plt.plot(hist.index, hist["Close"], linewidth=0.4)
+    plt.scatter(recomm.index, yValues, s=0.9, c="red")
     plt.show()
 
 my_button = Button(root, text="DO IT!", command=graph)
